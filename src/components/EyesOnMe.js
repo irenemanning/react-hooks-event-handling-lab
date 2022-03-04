23a8134ed87974f07c70fe5c32aa29a694ba49a9
Irene <irenemanningm@gmail.com>
2022-03-04 00:52:34 -0600

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,18 @@
 // Code EyesOnMe Component Here
+import react from "react";
+
+function EyesOnMe() {
+    function focus(){
+        return console.log("Good!")
+    }
+    function blur(){
+        return console.log("Hey! Eyes on me!")
+    }
+    return (
+        <div>
+            <button onFocus={focus} onBlur={blur}>Eyes on me</button>
+        </div>
+    )
+}
+
+export default EyesOnMe
